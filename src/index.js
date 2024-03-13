1e5cd0b3c8b09d600be979121dbb73a0bd1d171b
liamcottle <liam@liamcottle.com>
2024-03-13 21:02:58 +1300

    implement sidebar to show popular meshtastic devices

--- src/index.js
+++ src/index.js
@@ -84,4 +84,40 @@ app.get('/api/v1/nodes', async (req, res) => {
     }
 });
 
+app.get('/api/v1/stats/hardware-models', async (req, res) => {
+    try {
+
+        // get nodes from db
+        const results = await prisma.node.groupBy({
+            by: ['hardware_model'],
+            orderBy: {
+                _count: {
+                    hardware_model: 'desc',
+                },
+            },
+            _count: {
+                hardware_model: true,
+            },
+        });
+
+        const hardwareModelStats = results.map((result) => {
+           return {
+               count: result._count.hardware_model,
+               hardware_model: result.hardware_model,
+               hardware_model_name: HardwareModel.valuesById[result.hardware_model] ?? "UNKNOWN",
+           };
+        });
+
+        res.json({
+            hardware_model_stats: hardwareModelStats,
+        });
+
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({
+            message: "Something went wrong, try again later.",
+        });
+    }
+});
+
 app.listen(8080);
