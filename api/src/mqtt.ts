bdf618f57c754716209e42a0e95814b7ab339d72
Tilen Komel <me@komelt.dev>
2024-08-10 14:02:24 +0200

    Add mqtt client

---
+++ api/src/mqtt.ts
@@ -0,0 +1,20 @@
+import mqtt from 'mqtt';
+import { MQTT_URL, MQTT_USERNAME, MQTT_PASSWORD, MQTT_TOPIC } from './settings.js';
+
+export const mqttClient = mqtt.connect(MQTT_URL, {
+  username: MQTT_USERNAME,
+  password: MQTT_PASSWORD,
+});
+
+mqttClient.on("connect", () => {
+  mqttClient.subscribe(MQTT_TOPIC);
+  console.log(`
+    .- - - - - - - - - - - - - - - - - - - -
+    |   MQTT client listening on topic ${MQTT_TOPIC}
+    '- - - - - - - - - - - - - - - - - - - -
+    `)
+});
+
+mqttClient.on("error", (error) => {
+  console.error(error);
+});
