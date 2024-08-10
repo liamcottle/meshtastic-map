78856b9c742fd9f3b20d14fa6bd7e7598f58694b
Tilen Komel <me@komelt.dev>
2024-08-10 14:13:17 +0200

    Add index

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hej")
+import "./settings.js";
+import "./http.js";
+import "./express.js";
+import { mqttClient } from "./mqtt.js";
+
+import { handleStatMessage } from "./messages/stat.js";
+import { handleServiceEnvelope } from "./messages/service_envelope.js";
+
+mqttClient.on("message", async (topic, message) => {
+  try {
+    if (topic.includes("/stat/!"))
+      return await handleStatMessage(topic, message);
+
+    const data = await handleServiceEnvelope(topic, message);
+
+    if (!data) return;
+
+    const { packet, decodedPayload } = data;
+  } catch (err) {
+    console.error(err);
+  }
+});
