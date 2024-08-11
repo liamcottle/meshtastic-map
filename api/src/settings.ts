c54b3e697982b77f03ca6d41f4080c5bee8a19ad
Tilen Komel <me@komelt.dev>
2024-08-11 22:46:52 +0200

    Update settings

--- api/src/settings.ts
+++ api/src/settings.ts
@@ -14,9 +14,21 @@ export const COLLECT_TEXT_MESSAGES: boolean =
   !!process.env.COLLECT_TEXT_MESSAGES || false;
 export const COLLECT_POSITION: boolean =
   !!process.env.COLLECT_POSITION || false;
+export const COLLECT_NODEINFO: boolean =
+  !!process.env.COLLECT_NODEINFO || false;
+export const COLLECT_WAYPOINT: boolean =
+  !!process.env.COLLECT_WAYPOINT || false;
+export const COLLECT_NEIGHBOURINFO: boolean =
+  !!process.env.COLLECT_NEIGHBOURINFO || false;
+export const COLLECT_TRACEROUTE: boolean =
+  !!process.env.COLLECT_TRACEROUTE || false;
+export const COLLECT_MAP_REPOR: boolean =
+  !!process.env.COLLECT_MAP_REPOR || false;
 
 export const LOG_KNOWN_PACKET_TYPES: boolean =
   !!process.env.LOG_KNOWN_PACKET_TYPES || true;
+export const LOG_UNKNOWN_PACKET_TYPES: boolean =
+  !!process.env.LOG_UNKNOWN_PACKET_TYPES || false;
 
 export const DECRYPTION_KEYS: string[] = process.env.DECRYPTION_KEYS?.split(
   ","
