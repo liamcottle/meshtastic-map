6a4af1e22fa9c6eaa480bec44ba2c02f56af054f
Tilen Komel <me@komelt.dev>
2024-08-11 22:45:59 +0200

    Add waypoints

---
+++ api/src/messages/waypoint.ts
@@ -0,0 +1,61 @@
+import {
+  type MeshPacket,
+  type Data,
+  type Waypoint,
+  WaypointSchema,
+} from "@buf/meshtastic_protobufs.bufbuild_es/meshtastic/mesh_pb";
+import type { ServiceEnvelope } from "@buf/meshtastic_protobufs.bufbuild_es/meshtastic/mqtt_pb";
+import { fromBinary } from "@bufbuild/protobuf";
+import { prisma } from "../db";
+import { COLLECT_WAYPOINT, LOG_KNOWN_PACKET_TYPES } from "../settings";
+import { extractMetaData } from "../tools/decrypt";
+
+export async function handleWaypoint(
+  envelope: ServiceEnvelope,
+  packet: MeshPacket,
+  payload: Data
+): Promise<void> {
+  try {
+    const waypoint: Waypoint = fromBinary(WaypointSchema, payload.payload);
+
+    const { envelopeMeta, packetMeta, payloadMeta } = extractMetaData(
+      envelope,
+      packet,
+      payload
+    );
+
+    if (LOG_KNOWN_PACKET_TYPES) {
+      console.log("WAYPOINT_APP", {
+        envelopeMeta: envelopeMeta,
+        packetMeta: packetMeta,
+        payloadMeta: payloadMeta,
+        waypoint: waypoint,
+      });
+    }
+
+    if (COLLECT_WAYPOINT) {
+      await prisma.waypoint.create({
+        data: {
+          to: packet.to,
+          from: packet.from,
+          waypoint_id: waypoint.id,
+          latitude: waypoint.latitudeI,
+          longitude: waypoint.longitudeI,
+          expire: waypoint.expire,
+          locked_to: waypoint.lockedTo,
+          name: waypoint.name,
+          description: waypoint.description,
+          icon: waypoint.icon,
+          channel: packet.channel,
+          packet_id: packet.id,
+          channel_id: envelope.channelId,
+          gateway_id: envelope.gatewayId
+            ? BigInt(`0x${envelope.gatewayId.replaceAll("!", "")}`)
+            : null, // convert hex id "!f96a92f0" to bigint
+        },
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
