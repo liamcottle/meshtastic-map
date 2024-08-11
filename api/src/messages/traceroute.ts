50b5b9be8684e9153b0b8deb9313e6d068ef6fcd
Tilen Komel <me@komelt.dev>
2024-08-11 22:46:34 +0200

    Add traceroute

---
+++ api/src/messages/traceroute.ts
@@ -0,0 +1,58 @@
+import {
+  type MeshPacket,
+  type Data,
+  RouteDiscoverySchema,
+  RouteDiscovery,
+} from "@buf/meshtastic_protobufs.bufbuild_es/meshtastic/mesh_pb";
+import type { ServiceEnvelope } from "@buf/meshtastic_protobufs.bufbuild_es/meshtastic/mqtt_pb";
+import { fromBinary } from "@bufbuild/protobuf";
+import { prisma } from "../db";
+import { COLLECT_TRACEROUTE, LOG_KNOWN_PACKET_TYPES } from "../settings";
+import { extractMetaData } from "../tools/decrypt";
+
+export async function handleTraceroute(
+  envelope: ServiceEnvelope,
+  packet: MeshPacket,
+  payload: Data
+): Promise<void> {
+  try {
+    const traceroute: RouteDiscovery = fromBinary(
+      RouteDiscoverySchema,
+      payload.payload
+    );
+
+    const { envelopeMeta, packetMeta, payloadMeta } = extractMetaData(
+      envelope,
+      packet,
+      payload
+    );
+
+    if (LOG_KNOWN_PACKET_TYPES) {
+      console.log("TRACEROUTE_APP", {
+        envelopeMeta: envelopeMeta,
+        packetMeta: packetMeta,
+        payloadMeta: payloadMeta,
+        traceroute: traceroute,
+      });
+    }
+
+    if (COLLECT_TRACEROUTE) {
+      await prisma.traceRoute.create({
+        data: {
+          to: packet.to,
+          from: packet.from,
+          want_response: packet.wantAck,
+          route: traceroute.route,
+          channel: packet.channel,
+          packet_id: packet.id,
+          channel_id: envelope.channelId,
+          gateway_id: envelope.gatewayId
+            ? BigInt(`0x ${envelope.gatewayId.replaceAll("!", "")}`)
+            : null, // convert hex id "!f96a92f0" to bigint
+        },
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
