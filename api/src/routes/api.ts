68ea50d37eb34f892c658469477a1c4a673a378d
Tilen Komel <me@komelt.dev>
2024-08-16 08:49:58 +0200

    Added missing api routes

--- api/src/routes/api.ts
+++ api/src/routes/api.ts
@@ -1,14 +1,50 @@
 import express from "../express.js";
 
 express.get("/api", async (req, res) => {
-  const links = [
+  const links: { path: string; description: string }[] = [
     {
       path: "/api",
       description: "This page",
     },
     {
+      path: "/api/healthz",
+      description: "API health check.",
+    },
+    {
       path: "/api/v1/nodes",
       description: "Meshtastic nodes in JSON format.",
+    },
+    {
+      path: "/api/v1/nodes/:nodeId",
+      description: "Meshtastic node in JSON format.",
+    },
+    {
+      path: "/api/v1/nodes/:nodeId/environment-metrics",
+      description: "Meshtastic environment metrics in JSON format.",
+    },
+    {
+      path: "/api/v1/nodes/:nodeId/power-metrics",
+      description: "Meshtastic power metrics in JSON format.",
+    },
+    {
+      path: "/api/v1/nodes/:nodeId/mqtt-metrics",
+      description: "Meshtastic MQTT metrics in JSON format.",
+    },
+    {
+      path: "/api/v1/nodes/:nodeId/device-metrics",
+      description: "Meshtastic device metrics in JSON format.",
+    },
+    {
+      path: "/api/v1/nodes/:nodeId/neighbours",
+      description: "Meshtastic neighbours in JSON format.",
+    },
+    {
+      path: "/api/v1/nodes/:nodeId/traceroutes",
+      description: "Meshtastic traceroutes in JSON format.",
+    },
+    {
+      path: "/api/v1/text-messages",
+      description: "Meshtastic text messages in JSON format.",
     },
     {
       path: "/api/v1/stats/hardware-models",
