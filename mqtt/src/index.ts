b397215e24b3c2698c802152a66c9ead96ea1849
Tilen Komel <me@komelt.dev>
2024-08-19 22:08:42 +0200

    Added disabled note

--- mqtt/src/index.ts
+++ mqtt/src/index.ts
@@ -27,10 +27,7 @@ import { handleTelemetry } from "./messages/telemetry.js";
 import { handleTraceroute } from "./messages/traceroute.js";
 import { handleMapReport } from "./messages/map_report.js";
 
-import {
-  LOG_UNKNOWN_PACKET_TYPES,
-  PURGE_INTERVAL_SECONDS,
-} from "./settings.js";
+import { LOG_UNKNOWN_PACKET_TYPES, PURGE_INTERVAL_SECONDS } from "./settings.js";
 
 // run automatic purge if configured
 if (PURGE_INTERVAL_SECONDS !== 0) {
@@ -51,71 +48,77 @@ if (PURGE_INTERVAL_SECONDS !== 0) {
 
 // handle message received
 mqttClient.on("message", async (topic: string, message: Buffer) => {
-  try {
-    if (topic.includes("/stat/!")) {
-      // no need to continue with this mqtt message
-      return await handleStatMessage(topic, message);
-    }
+	try {
+		if (topic.includes("/stat/!")) {
+			// no need to continue with this mqtt message
+			return await handleStatMessage(topic, message);
+		}
 
-    const data = await handleServiceEnvelope(topic, message);
+		const data = await handleServiceEnvelope(topic, message);
 
-    // ignore if packet is empty
-    if (!data) return;
+		// ignore if packet is empty
+		if (!data) return;
 
-    const { envelope, packet, payload } = data;
+		const { envelope, packet, payload } = data;
 
-    // ignore if we can't decrypt the message
-    if (!payload) return;
+		// ignore if we can't decrypt the message
+		if (!payload) return;
 
-    const portnum = payload?.portnum;
+		const portnum = payload?.portnum;
 
-    switch (portnum) {
-      case 1:
-        await handleTextMessage(envelope, packet, payload);
-        break;
-      case 3:
-        await handlePosition(envelope, packet, payload);
-        break;
-      case 4:
-        await handleNodeInfo(envelope, packet, payload);
-        break;
-      case 8:
-        await handleWaypoint(envelope, packet, payload);
-        break;
-      case 67:
-        await handleTelemetry(envelope, packet, payload);
-        break;
-      case 70:
-        await handleTraceroute(envelope, packet, payload);
-        break;
-      case 71:
-        await handleNeighbourInfo(envelope, packet, payload);
-        break;
-      case 73:
-        await handleMapReport(envelope, packet, payload);
-        break;
-      default:
-        if (LOG_UNKNOWN_PACKET_TYPES) {
-          // ignore packets we don't want to see for now
-          if (
-            portnum === undefined || // ignore failed to decrypt
-            portnum === 0 || // ignore UNKNOWN_APP
-            portnum === 1 || // ignore TEXT_MESSAGE_APP
-            portnum === 5 || // ignore ROUTING_APP
-            portnum === 34 || // ignore PAXCOUNTER_APP
-            portnum === 65 || // ignore STORE_FORWARD_APP
-            portnum === 66 || // ignore RANGE_TEST_APP
-            portnum === 72 || // ignore ATAK_PLUGIN
-            portnum === 257 || // ignore ATAK_FORWARDER
-            portnum > 511 // ignore above MAX
-          ) {
-            return;
-          }
+		/*
+		Meshtastic removed the following portnums to be broadcasted to public
+		Meshtastic MQTT server. If you don't use deafult PSK, it doesn't effect you.
+		https://www.reddit.com/r/meshtastic/comments/1e00x1a/important_update_meshtastic_public_mqtt_server/
+		*/
 
-          console.log(portnum, envelope);
-        }
-    }
-  } catch (err) {
-    console.error(err);
-  }
+		switch (portnum) {
+			case 1: // TEXT_MESSAGE_APP
+				await handleTextMessage(envelope, packet, payload);
+				break;
+			case 3: // POSITION_APP
+				await handlePosition(envelope, packet, payload);
+				break;
+			case 4: // NODEINFO_APP
+				await handleNodeInfo(envelope, packet, payload);
+				break;
+			case 8: // WAYPOINT_APP - disabled
+				await handleWaypoint(envelope, packet, payload);
+				break;
+			case 67: // TELEMETRY_APP
+				await handleTelemetry(envelope, packet, payload);
+				break;
+			case 70: // TRACEROUTE_APP
+				await handleTraceroute(envelope, packet, payload);
+				break;
+			case 71: // NEIGHBOURINFO_APP - disabled
+				await handleNeighbourInfo(envelope, packet, payload);
+				break;
+			case 73: // MAP_REPORT_APP
+				await handleMapReport(envelope, packet, payload);
+				break;
+			default:
+				if (LOG_UNKNOWN_PACKET_TYPES) {
+					// ignore packets we don't want to see for now
+					if (
+						portnum === undefined || // ignore failed to decrypt
+						portnum === 0 || // ignore UNKNOWN_APP
+						portnum === 1 || // ignore TEXT_MESSAGE_APP
+						portnum === 5 || // ignore ROUTING_APP
+						portnum === 34 || // ignore PAXCOUNTER_APP
+						portnum === 65 || // ignore STORE_FORWARD_APP
+						portnum === 66 || // ignore RANGE_TEST_APP
+						portnum === 72 || // ignore ATAK_PLUGIN
+						portnum === 257 || // ignore ATAK_FORWARDER
+						portnum > 511 // ignore above MAX
+					) {
+						return;
+					}
+
+					console.log(portnum, envelope);
+				}
+		}
+	} catch (err) {
+		console.error(err);
+	}
 });
