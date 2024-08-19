177b01ecbbd09caf56815e41a1f8ab74ea786139
Tilen Komel <me@komelt.dev>
2024-08-19 22:05:39 +0200

    Updated neighbours

--- api/src/routes/neighbours.ts
+++ api/src/routes/neighbours.ts
@@ -2,68 +2,58 @@ import { prisma } from "../db.js";
 import express from "../express.js";
 
 express.get("/api/v1/nodes/:nodeId/neighbours", async (req, res) => {
-  try {
-    const nodeId = Number.parseInt(req.params.nodeId);
+	try {
+		const nodeId = Number.parseInt(req.params.nodeId);
 
-    // find node
-    const node = await prisma.node.findFirst({
-      where: {
-        node_id: nodeId,
-      },
-    });
+		// find node
+		const node = await prisma.node.findFirst({
+			where: {
+				node_id: nodeId,
+			},
+		});
 
-    // make sure node exists
-    if (!node) {
-      res.status(404).json({
-        message: "Not Found",
-      });
-      return;
-    }
+		// make sure node exists
+		if (!node) {
+			res.status(404).json({
+				message: "Not Found",
+			});
+			return;
+		}
 
-    // get nodes from db that have this node as a neighbour
-    const nodesThatHeardUs = await prisma.node.findMany({
-      where: {
-        neighbours: {
-          array_contains: {
-            node_id: Number(nodeId),
-          },
-        },
-      },
-    });
+		// get nodes from db that have this node as a neighbour
+		const nodesThatHeardUs = await prisma.node.findMany({
+			where: {
+				neighbours: {
+					array_contains: {
+						node_id: Number(nodeId),
+					},
+				},
+			},
+		});
 
-    console.log(node.neighbours);
-    console.log(node.neighbours_updated_at);
-
-    /*
-    if (node.neighbours) {
-      res.json({
-        nodes_that_we_heard: node.neighbours.map((neighbour) => {
-          return {
-            ..neighbour,
-            updated_at: node.neighbours_updated_at,
-          };
-        }),
-        nodes_that_heard_us: nodesThatHeardUs.map((nodeThatHeardUs) => {
-          const neighbourInfo = nodeThatHeardUs.neighbours.find(
-            (neighbour) =>
-              neighbour.node_id.toString() === node.node_id.toString()
-          );
-          return {
-            node_id: Number(nodeThatHeardUs.node_id),
-            snr: neighbourInfo.snr,
-            updated_at: nodeThatHeardUs.neighbours_updated_at,
-          };
-        }),
-      });
-    }
-    */
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      message: "Something went wrong, try again later.",
-    });
-  }
+		res.json({
+			nodes_that_we_heard: node.neighbours.map((neighbour) => {
+				return {
+					...neighbour,
+					updated_at: node.neighbours_updated_at,
+				};
+			}),
+			nodes_that_heard_us: nodesThatHeardUs.map((nodeThatHeardUs) => {
+				const neighbourInfo = nodeThatHeardUs.neighbours.find(
+					(neighbour) => neighbour.node_id.toString() === node.node_id.toString()
+				);
+				return {
+					node_id: Number(nodeThatHeardUs.node_id),
+					snr: neighbourInfo.snr,
+					updated_at: nodeThatHeardUs.neighbours_updated_at,
+				};
+			}),
+		});
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			message: "Something went wrong, try again later.",
+		});
+	}
 });
-console.log(
-  "API:EXPRESS registered route GET:/api/v1/nodes/:nodeId/neighbours"
-);
+console.log("API:EXPRESS registered route GET:/api/v1/nodes/:nodeId/neighbours");
