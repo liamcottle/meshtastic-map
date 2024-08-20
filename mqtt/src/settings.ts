fd6c6313ed2911e637359907cd8995c7fd29261a
liamcottle <liam@liamcottle.com>
2024-08-21 00:33:14 +1200

    implement purging waypoints

--- mqtt/src/settings.ts
+++ mqtt/src/settings.ts
@@ -41,6 +41,9 @@ export const PURGE_TRACEROUTES_AFTER_SECONDS: number = Number.parseInt(
 export const PURGE_WAYPOINTS_AFTER_SECONDS: number = Number.parseInt(
   process.env.PURGE_WAYPOINTS_AFTER_SECONDS || "0"
 );
+export const PURGE_WAYPOINTS_FOR_SECONDS: number = Number.parseInt(
+	process.env.PURGE_WAYPOINTS_FOR_SECONDS || "0"
+);
 
 export const COLLECT_SERVICE_ENVELOPES: boolean =
   !!process.env.COLLECT_SERVICE_ENVELOPES || false;
