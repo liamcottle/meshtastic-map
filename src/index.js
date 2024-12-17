8cc0d4bd83c69b2137f56dc7aea73364a031833b
liamcottle <liam@liamcottle.com>
2024-12-17 16:25:07 +1300

    update api docs endpoint

--- src/index.js
+++ src/index.js
@@ -95,8 +95,44 @@ app.get('/api', async (req, res) => {
             "description": "Meshtastic nodes in JSON format.",
         },
         {
+            "path": "/api/v1/nodes/:nodeId",
+            "description": "Meshtastic node info in JSON format.",
+        },
+        {
+            "path": "/api/v1/nodes/:nodeId/device-metrics",
+            "description": "Device metrics for a meshtastic node in JSON format.",
+        },
+        {
+            "path": "/api/v1/nodes/:nodeId/environment-metrics",
+            "description": "Environment metrics for a meshtastic node in JSON format.",
+        },
+        {
+            "path": "/api/v1/nodes/:nodeId/power-metrics",
+            "description": "Power metrics for a meshtastic node in JSON format.",
+        },
+        {
+            "path": "/api/v1/nodes/:nodeId/neighbours",
+            "description": "Neighbours for a meshtastic node in JSON format.",
+        },
+        {
+            "path": "/api/v1/nodes/:nodeId/traceroutes",
+            "description": "Trace Routes for a meshtastic node in JSON format.",
+        },
+        {
+            "path": "/api/v1/nodes/:nodeId/position-history",
+            "description": "Position history for a meshtastic node in JSON format.",
+        },
+        {
             "path": "/api/v1/stats/hardware-models",
             "description": "Database statistics about hardware models in JSON format.",
+        },
+        {
+            "path": "/api/v1/text-messages",
+            "description": "Meshtastic text messages in JSON format.",
+        },
+        {
+            "path": "/api/v1/text-messages/embed",
+            "description": "Meshtastic text messages rendered as an HTML page.",
         },
         {
             "path": "/api/v1/waypoints",
